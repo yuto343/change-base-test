6f1130c495d723f13efaa31fce0dc7690223c474
yuto343 <namawasabi1229@gmail.com>
2025-02-13 11:50:55 +0900

    chage base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  outDir: "dist/conference/",
+  base: "/conference",
   integrations: [mdx(), sitemap()],
 });
